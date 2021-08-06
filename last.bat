9456886e3f5441e418797fba211f0e02130f4161
Cno <dsyourshy@qq.com>
2021-08-06 19:29:12 +0800

    小改，能用水平

---
+++ last.bat
@@ -0,0 +1,9 @@
+title Edgeless Last Running...
+
+
+if "x%opt[Edgeless.opt_pin]%"=="xtrue" (
+  copy /y .\_vendor\File_PinIcons\00-InitPinIcons.lua "%x%\PEMaterial\Autoruns\Startup\"
+)
+
+
+title Edgeless Last Finished
