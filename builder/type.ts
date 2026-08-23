a846bb9ddb41032c61ad010da8b9b51aa2f5d352
Cno <dsyourshy@qq.com>
2026-08-23 14:49:26 +0800

    feat: 优化类型

--- builder/type.ts
+++ builder/type.ts
@@ -1,31 +1,54 @@
-// Form
-interface FormCtx {
-    val:string;
+// Command
+export interface FormCtx {
+    val: string;
 }
 
+export interface FormCommand {
+    command: (ctx: FormCtx) => string | string[];
+    commented?: boolean;
+}
+
+// Form
 interface BasicForm {
-    key:string;
-    label:string;
-    command: (ctx:FormCtx)=>string;
+    key: string;
+    label: string;
 }
 
 export interface CheckboxForm extends BasicForm {
-    type:'checkbox';
-    checked?:boolean;
+    type: 'checkbox';
+    checked?: boolean;
+    command: FormCommand;
 }
 
 export interface InputForm extends BasicForm {
-    type:'input';
-    defaultValue?:string;
+    type: 'input';
+    defaultValue?: string;
+    command: FormCommand;
 }
+
+export interface RadioOption {
+    label: string;
+    value: string;
+    command?: FormCommand;
+}
+
+export interface RadioForm extends BasicForm {
+    type: 'radio';
+    defaultValue: string;
+    options: RadioOption[];
+}
+
+export type Form = CheckboxForm | InputForm | RadioForm;
 
 // Structure
 export interface FormGroup {
-    label:string;
-    forms:BasicForm[];
+    type: 'group';
+    label?: string;
+    children: Array<Form | FormGroup>;
 }
+
 export interface FormPage {
-    page:string;
-    title:string;
-    groups:FormGroup[];
+    page: string;
+    title: string;
+    groups: FormGroup[];
 }
